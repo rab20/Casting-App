bfb5d62cdc8f6c59fccc24baa4641d2c48e6acf4
Bharat <ra.bharat2000@gmail.com>
2021-08-02 18:23:56 +0530

    Updated database path

--- database/models.py
+++ database/models.py
@@ -8,9 +8,9 @@ from sqlalchemy.sql.schema import ForeignKey
 from flask_migrate import Migrate
 from sqlalchemy.sql.sqltypes import Date, DateTime
 
-# database_path = os.environ['DATABASE_URL']
-database_name = "castingdb"
-database_path = "postgresql://{}:{}@{}/{}".format('postgres', 'postgres','localhost:5432', database_name)
+database_path = os.environ['DATABASE_URL']
+# database_name = "castingdb"
+# database_path = "postgresql://{}:{}@{}/{}".format('postgres', 'postgres','localhost:5432', database_name)
 
 db = SQLAlchemy()
 
